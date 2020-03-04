e8788a3fa0961406a9e49d3a75e247f78524a171
Laura Paajanen <laura.paajanen@cgi.com>
2020-03-04 17:00:53 +0200

    Add 1.9-1.15

---
+++ part1/1.15/react-app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import logo2 from './docker.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo2} className="App-logo" alt="logo" />
+          <h2>Welcome to React + Docker</h2>
+        </div>
+        <p className="App-intro">
+          <code>Create Dockerfile for an application in any of your own repositories and publish it to Docker Hub.</code>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
